73541f47f026ac3c9d6c6d9e730a3debf7629258
alicimen <alicimen1443@gmail.com>
2022-04-11 10:36:17 +0300

    day38: Inheritance Continue- Method Overriding

---
+++ src/day38_Inheritance/carTask/Toyota.java
@@ -0,0 +1,26 @@
+package day38_Inheritance.carTask;
+
+public class Toyota extends Car{
+
+    //Is there a way I can take out if there is any unnecessary parameters are given in constructor.
+    //Do I need an argument to set the brand?no, because all the cars from toyota class will have the same brand
+    //so the data is already known.We don't need an argument.The only time that you will pass the parameter
+    //when there is unknown information that must be given to the method
+    public Toyota(String model, int year, double price, String color, int miles) {
+        super("Toyota", model, year, price, color, miles);
+    }
+
+
+    public void reliable(){
+        System.out.println(brand+" "+model+" is reliable");
+    }
+
+
+}
+/*
+Create the following sub classes of Car:
+			1. Toyota:
+					extra methods:
+							reliabile()
+
+ */
