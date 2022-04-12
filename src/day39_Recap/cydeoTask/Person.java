ef123774399d9b2fb4eb5fca5fb2e566d935e947
alicimen <alicimen1443@gmail.com>
2022-04-12 14:11:57 +0300

    day39: Inheritance - Encapsulation Recap

---
+++ src/day39_Recap/cydeoTask/Person.java
@@ -0,0 +1,87 @@
+package day39_Recap.cydeoTask;
+
+public class Person {
+
+    private String name;
+    private int age;
+    private char gender;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        if(name.isBlank()||name.isEmpty()||name==null){
+            System.err.println("Invalid name");
+            System.exit(1);// 1 : something went wrong
+        }
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if(age<0 && age>150){
+            System.err.println("Invalid age");
+            System.exit(1);// 1 : something went wrong
+        }
+        this.age = age;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public void setGender(char gender) {
+        if( !( gender=='M'||gender=='F')  ){
+            System.err.println("Invalid gender");
+            System.exit(1);
+        }else if( !( gender=='m'||gender=='f')){
+            System.err.println("Invalid gender");
+        }
+        this.gender = gender;
+    }
+
+    public Person(String name, int age, char gender) {
+        setName(name);
+        setAge(age);
+        setGender(gender);
+    }
+
+    public void eat(){
+        System.out.println(name+" is eating");
+    }
+
+    public void drink(){
+        System.out.println(name+" is drinking");
+    }
+
+    public void sleep(){
+        System.out.println(name+" is sleeping");
+    }
+
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", gender=" + gender +
+                '}';
+    }
+}
+/*
+ 1. Create a class named Person:
+            Variables:
+                name, age, gender
+
+            Encapsulate all the fields
+
+            Add a constructor to set all the fields
+
+            Methods:
+                eat()
+                drink()
+                sleep()
+                toString()
+ */
