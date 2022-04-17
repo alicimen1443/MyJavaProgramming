31c3310f40d268d37de407726bb422bc9841ecbf
alicimen <alicimen1443@gmail.com>
2022-04-17 22:43:45 +0300

    day35_Practice task pizza object

--- src/day35_Encapsulation/encapsulation/PizzaObject.java
+++ src/day35_Encapsulation/encapsulation/PizzaObject.java
@@ -2,13 +2,41 @@ package day35_Encapsulation.encapsulation;
 
 import day35_Encapsulation.encapsulation.Pizza;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class PizzaObject {
     public static void main(String[] args) {
 
-        for (int i = 0; i <1 ; i++) {
-            Pizza pizza = new Pizza("medium",2,4);
+        ArrayList<Pizza> pizzas = new ArrayList<>();
+        // create 100 pizza object: size - 'S' , cheese topping 2, pepperoni topping - 3
+        // create 100 pizza object: size - 'M' , cheese topping 3, pepperoni topping - 4
+        // create 100 pizza object: size - 'L' , cheese topping 4, pepperoni topping - 5
 
+        for (int i = 0; i <100 ; i++) {
+            Pizza smallPizza = new Pizza("small",2,3);
+            Pizza mediumPizza = new Pizza("medium",3,4);
+            Pizza largePizza = new Pizza("large",4,5);
+
+            pizzas.addAll(Arrays.asList(smallPizza,mediumPizza,largePizza));
         }
+
+        System.out.println("Total number of pizzas :"+pizzas.size());
+
+        double totalPrice=0;
+
+        for(Pizza each : pizzas){
+            totalPrice+= each.calcCost();
+        }
+
+        System.out.println("total price = "+totalPrice);
+
+        Pizza pizza = new Pizza("medium",2,2);
+
+        pizza.setSize("large");
+        System.out.println(pizza);
+
+
 
 
 
