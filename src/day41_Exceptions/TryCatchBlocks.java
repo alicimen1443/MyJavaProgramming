1b7d58da1dccd7aaa43507e97535215e4d8af1b6
alicimen <alicimen1443@gmail.com>
2022-04-18 13:58:39 +0300

    day41_exceptions try catch blocks_ methods

--- src/day41_Exceptions/TryCatchBlocks.java
+++ src/day41_Exceptions/TryCatchBlocks.java
@@ -1,5 +1,8 @@
 package day41_Exceptions;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 public class TryCatchBlocks {
     public static void main(String[] args) {
 
@@ -50,6 +53,27 @@ public class TryCatchBlocks {
 
         }
         System.out.println("Test3 finished");
+
+        System.out.println("--------------------");
+
+        System.out.println("Hello World");
+
+
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Cydeo");
+
+        System.out.println("-----------------------------");
+
+        try {
+            FileInputStream file = new FileInputStream("File path");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
 
     }
 }
