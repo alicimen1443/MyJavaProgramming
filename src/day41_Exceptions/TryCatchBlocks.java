cfc34beb5d2564faba830d72fce260c660d87b22
alicimen <alicimen1443@gmail.com>
2022-04-18 13:32:58 +0300

    day41_exceptions try catch blocks

---
+++ src/day41_Exceptions/TryCatchBlocks.java
@@ -0,0 +1,55 @@
+package day41_Exceptions;
+
+public class TryCatchBlocks {
+    public static void main(String[] args) {
+
+        System.out.println("Test1 Started");
+
+        try{
+            System.out.println(9/0);
+
+            System.out.println("Try block");
+
+        }catch (ArithmeticException e){
+            System.out.println("Catch block");
+
+            System.out.println("Arithmetic Exception was occured");
+        }
+
+
+        System.out.println("Test1 Completed");
+
+        System.out.println("--------------------------------------------------");
+
+        System.out.println("Test2 started");
+
+        int[] numbers = {1,2,3,4,5};
+
+        try{
+            System.out.println(numbers[200]);
+            System.out.println("Try Block");
+
+        }catch (RuntimeException e){ // Everytime when the exceptions occur during the run time catch block tries to catch it
+// If we dont know what type of exception it is, then we pass Run time Exception because, it is the parent class of every runtime exceptions
+
+            e.printStackTrace();  // This method gives full details of my exception after completing the code fragment
+            //System.out.println(e.getMessage());
+        }
+
+        System.out.println("Test2 Completed");
+
+
+        System.out.println("--------------------------------------------------");
+
+        System.out.println("Test 3 started");
+
+        try{
+            System.out.println("Cydeo".substring(2,0));
+        }catch (RuntimeException e){
+            e.printStackTrace();
+
+        }
+        System.out.println("Test3 finished");
+
+    }
+}
