2519e64f83a04275ccc81d9e8e26623a6893700a
alicimen <alicimen1443@gmail.com>
2022-05-29 14:20:06 +0300

    day51_Collections

--- src/day51_Map/MapPractice2.java
+++ src/day51_Map/MapPractice2.java
@@ -42,13 +42,52 @@ public class MapPractice2 {
         System.out.println(name1);
         System.out.println(name2);
 
+        System.out.println("---------------------------------------------------------------");
+
+       // 1.3 how many employees has the salary between 120k ~ 150K?
+
+        int count = 0;
+
+        for (Integer eachValue : map.values()) {
+            if(eachValue>=120000 && eachValue<=150000){
+                count++;
+            }
+        }
+
+        System.out.println(count);
+
+        System.out.println("---------------------------------------------------------------");
+
+        //1.4 display the names of the employees who are making less than 118k?
+
+        for (Map.Entry<String, Integer> pair : map.entrySet()) {
+            if(pair.getValue()<118000){
+                System.out.println(pair.getKey());
+            }
+
+        }
+
+        System.out.println("---------------------------------------------------------------");
+
+        //1.5 increase the salary employee by 10K if the current salary of employee is less than 120K
+
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            if(entry.getValue()<120000){
+                entry.setValue(entry.getValue()+10000);
+            }
+        }
+        System.out.println(map);
+
+
+
+
 
 
 
     }
 }
 /*
-1. Given the following map that contains the employee name and thier salary:
+1. Given the following map that contains the employee name and their salary:
      Map<String, Integer> map = new LinkedHashMap<>();
         map.put("John", 123000);
         map.put("Antony", 100000);
